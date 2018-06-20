b1ff57e60f1f753f2a9356f0e51b3ca449772cb3
Epsian <jared.n.joseph@gmail.com>
2018-06-20 17:10:11 -0600

    Started network analyses and plots

---
+++ src/net_mes.R
@@ -0,0 +1,45 @@
+
+#### Data Load ####
+
+library(keyplayer)
+source("jj_model_dynamic.r")
+
+#### Setup ####
+
+last_net = net_list[[length(net_list)]]
+
+#### Remove Potential Ties From Dynet ####
+
+net_list_matrix_c = lappy(net_list_matrix, FUN = function(x){
+  x[x == 1] = 0
+  x[x == 2] = 0
+  x[x == -1] = 0
+  x[x == 3] = 1
+  return(x)
+})
+
+#### Graph Level ####
+
+gstats = data.frame(time = 1:length(net_list + 1),
+                    trans = tSnaStats(net_list_matrix_c, snafun='gtrans'),
+                    avg_degree = tSnaStats()
+                    )
+
+test = apply(tSnaStats(dynet, snafun='betweenness')[1:length(net_list_matrix_c),], 1, mean)
+
+/((num_authors-1)*(num_authors-2)/2)
+
+#### Vertex Level ####
+
+author_att$degree = degree(last_net, gmode="graph")
+author_att$kcore = kcores(last_net, mode="graph", diag= FALSE)
+author_att$avg_geo = (colSums(geodist(last_net)$gdist))/(nrow(as.sociomatrix(last_net)) - 1)
+author_att$nbet = (betweenness(last_net, gmode="graph", cmode="undirected")/((num_authors-1)*(num_authors-2)/2))
+author_att$evc = evcent(last_net, gmode = "graph", rescale = FALSE)
+author_att$frag = fragment(as.matrix.network.adjacency(last_net))
+
+
+
+
+
+
