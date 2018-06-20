327e9e5a54088d62adf819f80bbd28a8f38dfbd1
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2018-06-20 01:17:59 -0600

    choose_partner function

---
+++ choose_partner.r
@@ -0,0 +1,47 @@
+KLDivergence <- function(p,q){
+    # Computes the Kullback-Leibler divergence between p and q
+    div <- p*log2(p/q)
+    div[is.nan(div)] <- 0
+    return(sum(div))
+}
+
+JSDistance <- function(p,q){
+    # Computes the Jensen-Shannon distance between p and q
+    # Normalize the frequencies to create a probability distribution
+    p_norm <- p/sum(p)
+    q_norm <- q/sum(q)
+    m <- 0.5*(p_norm + q_norm)
+    divergence <- 0.5*KLDivergence(p_norm, m) + 0.5*KLDivergence(q_norm, m)
+    return(sqrt(divergence))
+}
+
+JSVector <- function(p,q_mat){
+    # Computes a vector of Jensen-Shannon distances between p
+    # and the rows of q_mat
+    js_vec <- apply(q_mat, 1, function(z) JSDistance(p,z))
+    return(js_vec)
+}
+
+choose_partner <- function(node_num, candidates, talent_matrix){
+    # Choose a node from among the candidates with probability
+    # proportional to the Jensen-Shannon distance to that node
+    # Step 1: JS distances
+    cur_talents <- talent_matrix[node_num,]
+    candidate_talents <- talent_matrix[candidates,]
+    distances <- JSVector(cur_talents, candidate_talents)
+    # Step 2: P(choose as partner) inversely proportional to distance
+    sims <- 1 - distances
+    sims_norm <- sims/sum(sims)
+    #print(sims_norm)
+    chosen <- sample(candidates, 1, prob=sims_norm)
+    #print(chosen)
+    return(chosen)
+}
+
+# Testing
+talents <- matrix(c(10,1,0,0,0,11,0,1,11,0,1,0), nrow=4, ncol=3, byrow=TRUE)
+node_num <- 1
+candidates <- c(2,3,4)
+choose_partner(node_num, candidates, talents)
+
+JSDistance(c(10,1,0),c(0,0,11))
