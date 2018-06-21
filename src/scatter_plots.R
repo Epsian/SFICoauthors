b879880bb57f689a07004b9cc7676ce3bcec8b09
Epsian <jared.n.joseph@gmail.com>
2018-06-21 02:11:35 -0600

    Updated Plots, bib

--- src/scatter_plots.R
+++ src/scatter_plots.R
@@ -57,13 +57,13 @@ decay_line <- plot_ly(x = decay_par, y = min_bet, type = 'scatter', mode = 'line
 
 # 
 
-netplot = plot_ly(gstats, x = colnames(gstats), y = 0, type = 'bar', name = 'Newman Model') %>%
-  add_trace(y = as.numeric(gstats[iter, ]), name = 'Model 1') %>%
-  layout(yaxis = list(title = 'Count'), barmode = 'group')
+#netplot = plot_ly(gstats, x = colnames(gstats), y = 0, type = 'bar', name = 'Newman Model') %>%
+  #add_trace(y = as.numeric(gstats[iter, ]), name = 'Model 1') %>%
+  #layout(yaxis = list(title = 'Count'), barmode = 'group')
 
 # Add newman model referance !!!
-top_talent = plot_ly(y = c(2, 8, 15), type = "box", color = c("newman", "Model 1")) %>%
-  add_trace(y = c(min(author_att$talent), median(author_att$talent), max(author_att$talent)))
+#top_talent = plot_ly(y = c(2, 8, 15), type = "box", color = c("newman", "Model 1")) %>%
+  #add_trace(y = c(min(author_att$talent), median(author_att$talent), max(author_att$talent)))
 
 #### Create Vector Plots ####
 # 
